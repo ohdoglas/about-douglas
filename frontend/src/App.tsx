8acbf916092f43cf6691affa625c5b614eb3e463
ohdoglas <dughlaisdd@gmail.com>
2024-12-23 08:27:33 -0300

    feature: app and index tsx created, public root html created

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+
+const App: React.FC = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route>
+
+                </Route>
+            </Routes>
+        </Router>
+    )
+}
+
+export default App;
